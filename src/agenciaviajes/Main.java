a240300c12627c8026e135c9dd8658eb78d2daac
Valentine Chimezie <Valentine Chimezie@PERSONAL>
2018-09-29 01:10:53 -0500

    Version final

--- src/agenciaviajes/Main.java
+++ src/agenciaviajes/Main.java
@@ -17,17 +17,28 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    AgenciaViajes prueba=new AgenciaViajes("Aviatur", 4, 4);
+    AgenciaViajes prueba=new AgenciaViajes("Aviatur",1,1);
     prueba.getHoteles(0).setNombre("Fontanar");
     prueba.getHoteles(0).setDireccion("Cra. 21a #159a-28");
     prueba.getViajes(0).setInicio("22/03/2017");
     prueba.getViajes(0).setDuracion("30 minutos");
+    
     prueba.getViajes(0).getSalida().setNoVuelo(1345);
-    prueba.getViajes(0).getSalida().setSalida("22/03/2017");
-    prueba.getViajes(0).getSalida().setLlegada("22/03/2017");
+    prueba.getViajes(0).getSalida().setDesde(prueba.getAeropuertos(0));
+    prueba.getViajes(0).getSalida().setHacia(prueba.getAeropuertos(1));
     prueba.getViajes(0).getSalida().getDesde().setDescripcion("José María Córdoba");
     prueba.getViajes(0).getSalida().getHacia().setDescripcion("Aeropuerto el dorado");
-    prueba.Listar(prueba.getViajes());
+    
+    prueba.getViajes(0).getSalida().setSalida("22/03/2017");
+    prueba.getViajes(0).getSalida().setLlegada("22/03/2017");
+    
+    prueba.getViajes(0).getRetorno().setHacia(prueba.getAeropuertos(0));
+    prueba.getViajes(0).getRetorno().setDesde(prueba.getAeropuertos(1));
+    
+     
+    prueba.getViajes(0).getRetorno().getDesde().setDescripcion("Aeropuerto el dorado");
+    prueba.getViajes(0).getRetorno().getHacia().setDescripcion("José María Córdoba");
+    prueba.Listar();
     }
     
 }
